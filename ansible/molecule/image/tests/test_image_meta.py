39a9c5a76df375108b6aa88d41ebe971e4189985
Stephan <stephan@takelwerk.net>
2021-10-14 18:07:14 +0200

    Remove env values test

--- ansible/molecule/image/tests/test_image_meta.py
+++ ansible/molecule/image/tests/test_image_meta.py
@@ -9,20 +9,6 @@ def test_image_meta_env_exists(image_meta_data):
     assert image_meta_data['Config']['Env'] is not None
 
 
-@pytest.mark.parametrize('env, value', [(0, 'DEBIAN_FRONTEND=noninteractive'),
-                                        (1, 'LANG=en_US.UTF-8'),
-                                        (2, 'SUPATH='),
-                                        (3, 'PATH='
-                                            '/usr/local/sbin:'
-                                            '/usr/local/bin:'
-                                            '/usr/sbin:'
-                                            '/usr/bin:'
-                                            '/sbin:'
-                                            '/bin')])
-def test_image_meta_env_values(image_meta_data, env, value):
-    assert value == image_meta_data['Config']['Env'][env]
-
-
 def test_image_meta_cmd(testvars, image_meta_data):
     image = os.environ.get('TAKELAGE_PROJECT_IMG')
     if 'command' in testvars['project']['images'][image]:
